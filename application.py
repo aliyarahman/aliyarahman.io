68e15d9758c9eabc8d9a3cb11fda5a9962da33d6
aliyarahman <aliya.rahman@yahoo.com>
2014-07-13 12:32:07 -0400

    Added skeleton for templating with d3 import

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 # Must be in a file called 'application.py' in order to work on Beanstalk
-import flask
+from flask import Flask, render_template
 
-application = flask.Flask(__name__) # Must be called 'application' in order to work on Beanstalk
-
-app.run(debug = True) # Comment this out upon deployment to Elastic Beanstalk
+application = Flask(__name__) # Must be called 'application' in order to work on Beanstalk
 
 @application.route('/')
-@application.route('/index')
 def index():
-  return ("Hello, Beanstalk")
+  return render_template("index.html")
+
+application.run(debug = True) # Comment this out upon deployment to Elastic Beanstalk
+
