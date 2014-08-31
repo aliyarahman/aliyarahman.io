96908aaa8cb8eebd254ab6027101e548831bf339
aliyarahman <aliya.rahman@yahoo.com>
2014-08-31 17:19:50 -0400

    First commit for v2

--- README.md
+++
@@ -1,5 +0,0 @@
-
-
-Uses AWS Elastic Beanstalk standards for deploying Flask app that renders d3 visualizations.
-
-Vanilla app stack, mostly. Chocolate viz.
--- application.py
+++
@@ -1,8 +0,0 @@
-# Must be in a file called 'application.py' in order to work on Beanstalk
-from flask import Flask, render_template
-
-application = Flask(__name__) # Must be called 'application' in order to work on Beanstalk
-
-@application.route('/')
-def index():
-  return render_template("index.html")
